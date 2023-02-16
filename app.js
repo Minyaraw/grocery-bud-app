aefcd665b32784d6febd9d7c0618a658ccfe93b7
Minyaraw <120593004+Minyaraw@users.noreply.github.com>
2023-02-16 02:06:39 -0500

    update app.js

--- app.js
+++ app.js
@@ -20,7 +20,32 @@ function addItem(e) {
 
     const id = new Date().getTime().toString();
     if (value && !editFlag ) {
-    console.log("add item to the list");
+    const element = document.createElement('article');
+    // add class
+    element.classList.add('grocery-item');
+    // add id
+    const attr = document.createAttribute('data-id');
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = `<p class="title">${value}</p>
+    <div class="btn-container">
+   <button type="button" class="edit-btn">
+       <i class="fas fa-edit"></i>
+   </button>
+   <button type="button" class="delete-btn">
+       <i class="fas fa-trash"></i>
+   </button>
+    </div> `;
+// append child
+list.appendChild(element);
+// display alert
+displayAlert("item added to the list", "success");
+// show container
+container.classList.add("show-container");
+// add to local storage
+addToLocalStorage(id, value);
+// set back to default
+setBackToDefault();
     } else if (value && editFlag ) {
     console.log("editing"); 
     } else {
@@ -34,10 +59,19 @@ alert.classList.add(`alert-${action}`);
 
 //remove alert
 setTimeout(function () {
-alert.textContent = '';
+alert.textContent = "";
 alert.classList.remove(`alert-${action}`);
-},1000);
+}, 1000);
+}
+// set back to default
+function setBackToDefault() {
+grocery.value = "";
+editFlag = false;
+editID = "";
+submitBtn.textContent = "submit";
 }
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id,value){
+console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
